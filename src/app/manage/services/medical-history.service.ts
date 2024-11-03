ac05f5bc61944cf1e940e7570be385319017c28f
feg06 <u202213468@upc.edu.pe>
2024-11-02 21:31:35 -0500

    feat(front-end) add login and register

---
+++ src/app/manage/services/medical-history.service.ts
@@ -0,0 +1,14 @@
+/*import { Injectable } from '@angular/core';
+import { BaseService } from '../../shared/services/base.service';
+import { MedicalHistory } from '../model/medical-history.entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicalHistoryService  extends BaseService<MedicalHistory>{
+  constructor() {
+    super();
+    this.resourceEndPoint = '/medicalHistory';
+  }
+}
+*/
