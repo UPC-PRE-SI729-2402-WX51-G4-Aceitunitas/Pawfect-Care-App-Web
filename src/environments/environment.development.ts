dd03828970f9bf202260a8f070d2ff5a7e503699
davidalexd <davidalexd1234@gmail.com>
2024-11-22 03:29:20 -0500

    update serverBasePath

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,3 @@ export const environments = {
   production: false,
   serverBasePath: 'https://pawfectcare-dnhqhvbmbxfgcqen.eastus2-01.azurewebsites.net',
 };
-//serverBasePath: 'https://pawfectcare-dnhqhvbmbxfgcqen.eastus2-01.azurewebsites.net',
