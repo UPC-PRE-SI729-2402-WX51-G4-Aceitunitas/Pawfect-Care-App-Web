cb243dd1456e022f6fe90ab081b2bbdf59ece397
davidalexd <davidalexd1234@gmail.com>
2024-11-22 02:31:06 -0500

    fix:merge styles login and register

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environments = {
   production: false,
-  serverBasePath: 'http://localhost:8080',
+  serverBasePath: 'https://pawfectcare-dnhqhvbmbxfgcqen.eastus2-01.azurewebsites.net',
 };
 //serverBasePath: 'https://pawfectcare-dnhqhvbmbxfgcqen.eastus2-01.azurewebsites.net',
